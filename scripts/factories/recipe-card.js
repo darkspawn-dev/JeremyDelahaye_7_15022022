4e17d91f7eab431a848f562f58b3bbeb1413cd03
Jerem <jdelahaye@xee.com>
2022-02-17 19:24:54 +0100

    Suite base

---
+++ scripts/factories/recipe-card.js
@@ -0,0 +1,61 @@
+export class RecipeCard {
+  constructor(title, ingredients, description, duration) {
+    this.title = title;
+    this.ingredients = ingredients;
+    this.description = description;
+    this.duration = duration;
+  }
+
+  render() {
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+
+    const img = document.createElement("img");
+    img.setAttribute("class", "card-img-top");
+    card.appendChild(img);
+    
+    const cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+    card.appendChild(cardBody);
+
+    const h5 = document.createElement("h5");
+    h5.setAttribute("class", "card-title");
+    // h5.nodeValue = this.title;
+    h5.textContent = this.title;
+    cardBody.appendChild(h5);
+
+    const duration = document.createElement("span");
+    duration.setAttribute("class", "oi oi-clock");
+    cardBody.appendChild(duration);
+    // duration.nodeValue = `${this.duration}min`;
+    duration.textContent = `${this.duration}min`;
+
+    const ingredients = document.createElement("div");
+    ingredients.setAttribute("class", "ingredients");
+    cardBody.appendChild(ingredients)
+
+    const ul = document.createElement("ul");
+    ingredients.appendChild(ul);
+
+    this.ingredients.forEach((ingredient) => {
+      const li = document.createElement("li");
+      ul.appendChild(li);
+
+      const span = document.createElement("span");
+      span.setAttribute("class", "ingredients-list");
+      // span.nodeValue = ingredient.ingredient;
+      span.textContent = ingredient.ingredient;
+      li.appendChild(span);
+      li.nodeValue = `${ingredient.quantity}${ingredient.unit}`;
+    });
+
+    // description a faire
+    const description = document.createElement("div");
+    description.setAttribute("class", "description");
+    description.textContent = this.description
+    cardBody.appendChild(description);
+    
+    return card
+  }
+  
+}
